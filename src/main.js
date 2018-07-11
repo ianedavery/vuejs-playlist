4c458b55ef6098d87c9d3e5b5b0e38cec365a7a3
Ian Avery <averi@swift.com>
2018-07-11 16:05:59 -0700

    created vue app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
+
+Vue.component("app-header", Header);
+Vue.component("app-footer", Footer);
+Vue.component("app-ninjas", Ninjas);
+
+export const bus = new Vue();
 
 new Vue({
   el: '#app',
